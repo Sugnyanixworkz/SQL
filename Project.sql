cf473b54814a0ea3cefeaf0a5ac841e41fe1f03c
Dr.nagaraj Gandigudi <sugnyani.xworkz@gmail.com>
2024-06-27 09:46:33 +0530

    26-06-2024

---
+++ Project.sql
@@ -0,0 +1,15 @@
+create database company;
+create database Employee;
+create database Restaurent;
+create database Apartment;
+create database Hospital;
+create database Club;
+
+create table Department;
+CREATE database Project;
+CREATE table Airport_details(id int ,airport_name varchar(20),arrival int ,departure int,price int);
+
+CREATE Table Cricket_teams(id int ,team_name varchar(20),no_of_players int ,captain varchar(10),coach varchar(10));
+USE project;
+CREATE table Airport_details(id int ,airport_name varchar(20),arrival int ,departure int,price int);
+CREATE Table Cricket_teams(id int ,team_name varchar(20),no_of_players int ,captain varchar(10),coach varchar(10));
